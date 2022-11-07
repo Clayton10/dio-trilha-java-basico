70a0fea7420165855cf32c6b10c7842451d5889c
Clayton10 <queito_csn@hotmail.com>
2022-11-07 13:47:23 -0300

    Commitando

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -8,6 +8,8 @@ public class TiposVariaveis {
         int numero              = 5;
         numero                  = 10;
         System.out.println(numero);
+        final double VALOR_DE_PI      = 3.14;
+        VALOR_DE_PI             = 10.75;
 
 
     }
