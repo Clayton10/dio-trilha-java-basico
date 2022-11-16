1494e2b149fc5abf29e40765f29ff87acacef466
Clayton10 <queito_csn@hotmail.com>
2022-11-16 20:13:31 -0300

    Commitando o código da tabuada.

---
+++ ex5_tabuada/Ex5_Tabuada/src/Ex5_Tabuada.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Ex5_Tabuada{
+  public static void main(String[] args) {
+    Scanner scan  = new Scanner(System.in);
+
+    System.out.print("Informe a tabuada: ");
+    int tabuada   = scan.nextInt();
+
+    System.out.println("\nTabuada do "+ tabuada +"\n");
+    for (int i = 1; i <= 10; i++){
+      System.out.println(tabuada + " x " + i + " = "+ (tabuada*i));
+    }
+
+    
+  }
+}
